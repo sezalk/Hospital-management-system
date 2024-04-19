984c354106f879630c1770aea72570bebc85de77
hauntedguest <99336943+hauntedguest@users.noreply.github.com>
2024-04-19 23:56:04 +0530

    appointment continued

---
+++ Backend/router/appointmentRouter.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { getAllAppointments, postAppointment, updateAppointmentStatus } from "../controller/appointmentController.js";
+import {isAdminAuthenticated,isPatientAuthenticated} from "../middlewares/auth.js"
+const router = express.Router();
+
+router.post("/post", isPatientAuthenticated, postAppointment);
+router.get("/getall", isAdminAuthenticated, getAllAppointments);
+router.put("/update/:id", isAdminAuthenticated, updateAppointmentStatus);
+
+export default router;
