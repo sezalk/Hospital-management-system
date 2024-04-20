e3ad1b5ff751af181aba80b590842472ddd0105f
hauntedguest <99336943+hauntedguest@users.noreply.github.com>
2024-04-20 13:10:31 +0530

    error correction

--- Backend/models/userSchema.js
+++ Backend/models/userSchema.js
@@ -59,11 +59,11 @@ const userSchema= new mongoose.Schema({
     docAvatar:{
         public_id: {
             type: String, 
-            required: true,
+            //required: true,
           },
           url: {
             type: String, 
-            required: true,
+            //required: true,
           },
       
     }
