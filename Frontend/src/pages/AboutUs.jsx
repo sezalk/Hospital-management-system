8a2ee1959fa2adf105e91ea7bdcd267c5f161638
hauntedguest <99336943+hauntedguest@users.noreply.github.com>
2024-04-20 13:56:03 +0530

    about us

--- Frontend/src/pages/AboutUs.jsx
+++ Frontend/src/pages/AboutUs.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
+import React from "react";
+import Hero from "../components/Hero";
+import Biography from "../components/Biography";
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
-  )
-}
+    <>
+      <Hero
+        title={"Learn More About Us | JDR Medical Institute"}
+        imageUrl={"/about.png"}
+      />
+      <Biography imageUrl={"/whoweare.png"} />
+    </>
+  );
+};
 
-export default AboutUs
+export default AboutUs;
