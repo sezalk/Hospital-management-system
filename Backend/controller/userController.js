79dafe984f9cf7446e78450d7634a47f55f1a3c0
hauntedguest <99336943+hauntedguest@users.noreply.github.com>
2024-04-21 23:19:17 +0530

    error and addnew doctor

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -117,49 +117,78 @@ export const logoutPatient = catchAsyncErrors(async(req,res,next)=>{
     });
 });
 
-export const addNewDoctor = catchAsyncErrors(async(req,res,next)=>{
-    if(!req.files || Object.keys(req.files).length === 0){
-        return next(new ErrorHandler("Doctor Avatar Required!",400));
+export const addNewDoctor = catchAsyncErrors(async (req, res, next) => {
+    if (!req.files || Object.keys(req.files).length === 0) {
+      return next(new ErrorHandler("Doctor Avatar Required!", 400));
     }
-    const {docAvatar} =req.files;
+    const { docAvatar } = req.files;
     const allowedFormats = ["image/png", "image/jpeg", "image/webp"];
-    if(!allowedFormats.includes(docAvatar.mimetype)){
-        return next(new ErrorHandler("File Format Not Supported!",400));
+    if (!allowedFormats.includes(docAvatar.mimetype)) {
+      return next(new ErrorHandler("File Format Not Supported!", 400));
     }
-    const {firstName, lastName, email, phone, nic, dob, gender, password , doctorDepartment} = req.body;
-    if(
-        !firstName ||
-        !lastName ||
-        !email ||
-        !phone ||
-        !nic ||
-        !dob ||
-        !gender ||
-        !password ||
-        !doctorDepartment
-    ){
-        return next(new ErrorHandler("Please Provide Full Details!",400));
+    const {
+      firstName,
+      lastName,
+      email,
+      phone,
+      nic,
+      dob,
+      gender,
+      password,
+      doctorDepartment,
+    } = req.body;
+    if (
+      !firstName ||
+      !lastName ||
+      !email ||
+      !phone ||
+      !nic ||
+      !dob ||
+      !gender ||
+      !password ||
+      !doctorDepartment ||
+      !docAvatar
+    ) {
+      return next(new ErrorHandler("Please Fill Full Form!", 400));
     }
-    const isRegistered = await User.findOne({email});
-    if(isRegistered){
-        return next(new ErrorHandler(`${isRegistered.role} already registered with this Email!`,400));
+    const isRegistered = await User.findOne({ email });
+    if (isRegistered) {
+      return next(
+        new ErrorHandler("Doctor With This Email Already Exists!", 400)
+      );
     }
-
-    const cloudinaryResponse = await cloudinary.uploader.upload(docAvatar.tempFilePath);
-    if(!cloudinaryResponse || cloudinaryResponse.error){
-        console.error("Cloudinary Error:",cloudinaryResponse.error || "Unknown Cloudinary Error");
+    const cloudinaryResponse = await cloudinary.uploader.upload(
+      docAvatar.tempFilePath
+    );
+    if (!cloudinaryResponse || cloudinaryResponse.error) {
+      console.error(
+        "Cloudinary Error:",
+        cloudinaryResponse.error || "Unknown Cloudinary error"
+      );
+      return next(
+        new ErrorHandler("Failed To Upload Doctor Avatar To Cloudinary", 500)
+      );
     }
-
     const doctor = await User.create({
-        firstName, lastName, email, phone, nic, dob, gender, password , doctorDepartment,role:"Doctor",docAvatar:{
-            public_id: cloudinaryResponse.public_id,
-            url: cloudinaryResponse.secure_url,
-        },
-
+      firstName,
+      lastName,
+      email,
+      phone,
+      nic,
+      dob,
+      gender,
+      password,
+      role: "Doctor",
+      doctorDepartment,
+      docAvatar: {
+        public_id: cloudinaryResponse.public_id,
+        url: cloudinaryResponse.secure_url,
+      },
     });
     res.status(200).json({
-        success:true,
-        message:"New Doctor Registered!",
-        doctor
+      success: true,
+      message: "New Doctor Registered",
+      doctor,
     });
-})
+  });
+  
